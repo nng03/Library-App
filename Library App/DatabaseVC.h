6a928cc3c3000bd240664763f01eb3e8a215067d
nng03 <nn733@nyu.edu>
2014-04-21 16:14:19 -0400

    Start Database stuff
    stuff

---
+++ Library App/DatabaseVC.h
@@ -0,0 +1,13 @@
+//
+//  DatabaseVC.h
+//  Library App
+//
+//  Created by fp on 4/18/14.
+//  Copyright (c) 2014 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DatabaseVC : UITableViewController
+
+@end
---
+++ Library App/DatabaseVC.m
@@ -0,0 +1,52 @@
+//
+//  DatabaseVC.m
+//  Library App
+//
+//  Created by fp on 4/18/14.
+//  Copyright (c) 2014 Nick. All rights reserved.
+//
+
+#import "DatabaseVC.h"
+
+@interface DatabaseVC ()
+
+@end
+
+@implementation DatabaseVC
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return 0;
+}
+
+@end
